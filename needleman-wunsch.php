7210a04d24a48cd79826f79ac4b155acdc1ef18a
MatheusMNS <matheus_moraes_nunes@hotmail.com>
2018-12-16 12:14:04 -0200

    Add files via upload

--- needleman-wunsch.php
+++ needleman-wunsch.php
@@ -1,12 +1,13 @@
 <?php 
-	echo "hello world";
-
 	$penalidade_gap = -1;   // Gap Penalty
-	$pontuacao = 1;         // Match Score
+	$pontuacao_match = 1;         // Match Score
 	$incompatibilidade = 0; // Mismatch Score
-	$sequencia1 = "aaaaaaaaaaa";
-	$sequencia2 = "aaaaaaaaaaa";
+	$sequencia1 = "GTG";
+	$sequencia2 = "GTG";
+	$alinhamentoA = "";
+	$alinhamentoB = "";
 	$matriz = array();
+	$alinhamento_otimo = array();
 	
 	// Inicializando a matriz
 	for($i = 0; $i < strlen($sequencia1)+1; $i++) {
@@ -24,7 +25,7 @@
 	
 	for($i = 1; $i < strlen($sequencia1); $i++) {
 		for($j = 1; $j < strlen($sequencia2); $j++) {
-			$pon_inc = ($sequencia1[$i-1] === $sequencia2[$j-1]) ? $pontuacao : $incompatibilidade;
+			$pon_inc = ($sequencia1[$i-1] === $sequencia2[$j-1]) ? $pontuacao_match : $incompatibilidade;
 			$escolha1 = $matriz[$i-1][$j-1] + $pon_inc;
 			$escolha2 = $matriz[$i-1][$j] + $penalidade_gap;
 			$escolha3 = $matriz[$i][$j-1] + $penalidade_gap;
@@ -33,7 +34,64 @@
 		}
 		echo "<html><br /></html>";
 	}
+	/***********************************************************************/
+	$i = strlen($sequencia1);
+	$j = strlen($sequencia2);
 	
-	
+	$alinhamento_otimo['sequencia1'] = array();
+	$alinhamento_otimo['sequencia2'] = array();
+	$alinhamento_otimo['aln'] = array();
+	$alinhamento_otimo['score'] = $matriz[$i][$j];
+
+	while($i > 0 && $j > 0){
+		$score = $matriz[$i][$j];
+		$scoreDiag = $matriz[$i-1][$j-1];
+		$scoreUp = $matriz[$i][$j-1];
+		$scoreLeft = $matriz[$i-1][$j];
+		$pon_inc = ($sequencia1[$i-1] === $sequencia2[$j-1]) ? $pontuacao_match : $incompatibilidade;
+		
+		if($score == $scoreDiag + $pon_inc){
+			$alinhamento_otimo['sequencia1'][] = $sequencia1[$i-1];
+			$alinhamento_otimo['sequencia2'][] = $sequencia2[$j-1];
+			$alinhamento_otimo['aln'][] = ($sequencia1 === $sequencia2) ? '|' : ' ';
+			$i--;
+			$j--;
+			echo $sequencia1[$i-1];
+		}
+		else if($score == ($scoreLeft + $penalidade_gap)){
+			$alinhamento_otimo['sequencia1'][] = $sequencia1[$i-1];
+			$alinhamento_otimo['sequencia2'][] = '-';
+			$alinhamento_otimo['aln'][] = ' ';
+			$i--;
+		}
+		else if($score == ($scoreUp + $penalidade_gap)){
+			$alinhamento_otimo['sequencia1'][] = '-';
+			$alinhamento_otimo['sequencia2'][] = $sequencia2[$j-1];
+			$alinhamento_otimo['aln'][] = ' ';
+			$j--;
+		}
+	}
+	/*
+	echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+	echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head>';
+	echo '<meta http-equiv="content-type" content="text/html;charset=utf-8" />';
+	echo '<meta name="description" content="" />';
+	echo '<meta name="keywords" content="" />';
+	echo '<title>Needleman-Wunsch Alignment Score Table</title>';
+	echo '</head>';
+	echo '<body>';
+	echo '<h3>Optimal Global Alignment (score = '.$this->optimal_alignment['score'].')</h3>';
+	echo '<table class="align">';
+	foreach(array('seq2', 'aln', 'seq1') as $k) {
+		echo '<tr>';
+		foreach($this->optimal_alignment[$k] as $v) {
+			echo "<td>$v</td>";
+		}
+		echo '</tr>';
+	}
+	echo "\n</table>";
+
+	echo '</body></html>';
+	*/
 	
 ?>
